b246bdc0c837ab5d5ea9cf1f6d1497e34f86a479
AcePNG <ace.gjorgjievski6@gmail.com>
2023-01-26 21:53:11 +0100

    heart.py message written commented

--- pythonProject/heart.py
+++ pythonProject/heart.py
@@ -1,12 +1,14 @@
 from turtle import *
 
+# Do NOT run this :")
+
 bgcolor("cyan")
 color("red")
 begin_fill()
 pensize(10)
 left(50)
 forward(133)
-circle(50,200)
+circle(50, 200)
 right(140)
 circle(50, 200)
 forward(133)
